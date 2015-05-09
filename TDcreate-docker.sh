2ae14a326712dba929b0d4253b7c7dbed1a9577b
Ignacio Pascual <ipascual@unexpectedit.com>
2015-05-09 12:41:04 -0500

    dynamic folder

--- TDcreate-docker.sh
+++ TDcreate-docker.sh
@@ -2,17 +2,19 @@
 
 docker ps -a
 echo "- - -"
-echo -n "Enter name of container (CID) e.g. jenkins001: "
+echo -n "Enter name of container (CID) e.g. jenkins001 :"
 read CID
-echo -n "Enter ports param. e.g. -p 8082:8080 -p 4022:22: "
+echo -n "Enter ports param. e.g. -p 8082:8080 -p 4022:22 :"
 read PORTS
-echo -n "Enter docker image. e.g. jdubois/jhipster-docker: "
+echo -n "Enter docker image. e.g. jdubois/jhipster-docker :"
 read IMAGE
+echo -n "Enter docker shared folder. e.g. /var/jenkins_home :"
+read FOLDER_DOCKER
 
 mkdir -p /docker/$CID
 chmod 777 -R /docker/$CID
 chown 1000 /docker/$CID
 
-COMMAND="docker create --privileged=true --name $CID -v /docker/$CID:/var/jenkins_home $PORTS -t $IMAGE"
+COMMAND="docker create --privileged=true --name $CID -v /docker/$CID:$FOLDER_DOCKER $PORTS -t $IMAGE"
 echo $COMMAND
 echo $($COMMAND)
